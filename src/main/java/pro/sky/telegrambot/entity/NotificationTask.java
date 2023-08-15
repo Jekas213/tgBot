d2b06bee487dcc88d743065af7848cf720d0c4a6
Dmitriy <jekas213@gmail.com>
2023-08-15 22:28:04 +0300

    end

---
+++ src/main/java/pro/sky/telegrambot/entity/NotificationTask.java
@@ -0,0 +1,58 @@
+package pro.sky.telegrambot.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class NotificationTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    private long chatId;
+
+    private LocalDateTime dateTime;
+
+    public NotificationTask(String text, long chatId, LocalDateTime dateTime) {
+        this.text = text;
+        this.chatId = chatId;
+        this.dateTime = dateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+}
--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -8,18 +8,28 @@ import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.service.NotificationTaskService;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private static final Pattern PATTERN = Pattern.compile("(\\d{1,2}\\.\\d{1,2}\\.\\d{4} \\d{1,2}:\\d{2}) ([А-яA-z\\d,\\s?!:.]+)");
 
     @Autowired
     private TelegramBot telegramBot;
+    @Autowired
+    private NotificationTaskService notificationTaskService;
 
     @PostConstruct
     public void init() {
@@ -30,9 +40,40 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
+            long chatId = update.message().chat().id();
+            String text = update.message().text();
+            Matcher matcher = PATTERN.matcher(text);
+            if ("/start".equals(text)) {
+                sendMessage(chatId, "Hello");
 
+            } else if (matcher.matches()) {
+                LocalDateTime dateTime = pars(matcher.group(1));
+                String message = matcher.group(2);
+                assert dateTime != null;
+                notificationTaskService.save(chatId, message, dateTime);
+                sendMessage(chatId, "Задача записана");
+            } else {
+                sendMessage(chatId, "введите /start либо задачу в формате дд.мм.гггг вв:вв Задача");
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+    @Nullable
+    private LocalDateTime pars(String dateTime) {
+        try {
+            return LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+
+    private void sendMessage(long chatId, String text) {
+        SendMessage message = new SendMessage(chatId,
+                text);
+        SendResponse response = telegramBot.execute(message);
+        if (!response.isOk()) {
+            logger.error("failed: " + response.description());
+        }
+    }
 }
